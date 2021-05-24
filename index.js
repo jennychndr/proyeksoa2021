3b5cef9ea4f528af0e8a011730538fda97dbf1ac
weifang28 <marvellal2802@gmail.com>
2021-05-24 08:47:01 +0700

    new file, install & init
    +blind code "homepage"

---
+++ index.js
@@ -0,0 +1,15 @@
+//sdh install express mysql multer axios jwt
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(3000, function(){
+    console.log("3000 hehehe.");
+});
+
+const users = require("./routes/users");
+app.use("/api/users",users);
+
+const plant = require("./routes/plant");
+app.use("/api/plant",plant);
